f3d7a365e9654db4f8b9cdb7a0b70de3c6192a68
DeeGeeWest <DeeGeeWest@gmail.com>
2022-03-09 17:59:15 -0500

    add all dragons both ways

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,10 @@
 class ProductsController < ApplicationController
   def get_one_product
     product = Product.last
-    # render json: {id: contact.id, title: contact.title, chef: contact.chef}
     render json: product.as_json
   end
 
-  def get_all_productss
+  def get_all_products
     product = Product.all
     render json: product.as_json
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,15 @@
 #
 # Examples:
 #
-   product = Product.create(name: "Shadrix Silverquill", price: "3.59", iamage_url: "https://cdn.ionsuite.com/image/ion-prime/data/mtg/stx/235173_200w-800x800.jpg", description: "Shadrix Silverquill is one of the five elder dragons from Arcavios that founded Strixhaven. The {W}{B} Silverquill College is named in his honor.")
+   product = Product.create(name: "Shadrix Silverquill", price: "3.59", image_url: "https://cdn.ionsuite.com/image/ion-prime/data/mtg/stx/235173_200w-800x800.jpg", description: "Shadrix Silverquill is one of the five elder dragons from Arcavios that founded Strixhaven. The {W}{B} Silverquill College is named in his honor.")
+   product.save
+
+   product = Product.create(name: "Velomachus Lorehold", price: "6.00", image_url: "https://product-images.tcgplayer.com/fit-in/400x558/235635.jpg", description: "Velomachus Lorehold is one of the five elder dragons from Arcavios that founded Strixhaven. The {R}{W} Lorehold College is named in her honor. It is said she was once cured of a secret illness by Dean Lisette of Witherbloom College.")
+   product.save
+
+   product = Product.create(name: "Galazeth Prismari", price: "5.68", image_url: "https://product-images.tcgplayer.com/fit-in/400x558/235853.jpg", description: "Galazeth Prismari is one of the five elder dragons from Arcavios that founded Strixhaven. He has a flair for the dramatic and the {U}{R} Prismari College is named in his honor.")
+   product.save
+
+   product = Product.create(name: "Tanazir Quandrix", price: "2.49", image_url: "https://product-images.tcgplayer.com/fit-in/368x368/235664.jpg", description: "Tanazir Quandrix is one of the five elder dragons from Arcavios that founded Strixhaven. The {G}{U} Quandrix College is named in her honor.")
    product.save
   
