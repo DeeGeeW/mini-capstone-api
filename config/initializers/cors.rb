3d6cfe487957e022b4a860f2e842f018e9c47081
DeeGeeWest <DeeGeeWest@gmail.com>
2022-04-20 21:26:20 -0400

    add frontend to cors via netify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "mini-capstone.peterxjang.com", "localhost:8080"
+    origins "example.com", "mini-capstone.peterxjang.com", "localhost:8080", "silver-bunny-b4229f.netlify.app"
 
     resource "*",
       headers: :any,
