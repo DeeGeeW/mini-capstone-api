34f9ccf68cd5a659803b820803f5913fcd380675
DeeGeeWest <DeeGeeWest@gmail.com>
2022-06-28 22:00:00 -0400

    carted or category get down to what is what

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,9 @@
 class Order < ApplicationRecord
-  
   belongs_to :user
   has_many :carted_products
   has_many :products, through: :carted_products
-  
+  # has_many :category_product
+
   # belongs_to :product
- 
 
 end
